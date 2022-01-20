6815786aa8e990e002ff72a359b1f1605a0067ef
peti <petikoch@gmail.com>
2022-01-20 18:05:20 +0100

    update versions of libs + gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id( "com.github.ben-manes.versions") version "0.39.0"
+    id( "com.github.ben-manes.versions") version "0.41.0"
 }
 
 java {
@@ -27,9 +27,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.assertj:assertj-core:3.21.0")
-    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.mockito:mockito-core:4.2.0")
     // testImplementation("org.mockito:mockito-junit-jupiter:2.+")
 }
 
